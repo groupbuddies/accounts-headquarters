0f97be38e717869fca76f4cd7fb1ee3226743c4f
Gabriel Poça <gabrielpoca@gmail.com>
2015-07-23 10:16:26 +0100

    Update demo meteor version and require headquarters-meteor version

--- package.js
+++ package.js
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
   api.use('accounts-ui', ['client', 'server']);
-  api.use('gabrielpoca:headquarters', ['client', 'server']);
+  api.use('gabrielpoca:headquarters@=0.0.4', ['client', 'server']);
 
   api.use('http', ['client', 'server']);
 
